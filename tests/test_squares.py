c06d15c72a7620dd40a96d169f533e775e69c31e
[green-rose-ds] <[roberts.rosea@nulondon.ac.uk]>
2026-04-20 18:46:46 +0100

    add to README

--- tests/test_squares.py
+++ tests/test_squares.py
@@ -4,5 +4,9 @@ class SmokeTest(unittest.TestCase):
     def test2_and2(self):
         self.assertEqual(2+2,4)
 
+class TestSquare(unittest.TestCase):
+    def test_root_of0(self):
+        self.assertEqual(sq_num(0),0)
+
 if __name__ == "main":
     unittest.main()
